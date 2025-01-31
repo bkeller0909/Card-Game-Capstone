3c90530a258d527a3c06505376ba63d3bb34498c
Cooper Hill <cooperhill2004@gmail.com>
2025-01-30 20:27:39 -0500

    Updated Spell States
    Updated the spell unique spell states and added a get random finger

--- GraspingChaos_Build/Assets/Scripts/State Scripts/2 Attack Spells/RockThrowState.cs
+++ GraspingChaos_Build/Assets/Scripts/State Scripts/2 Attack Spells/RockThrowState.cs
@@ -1,28 +1,63 @@
 public class RockThrowState : FSMState
 {
     PlayerState playerState;
+    private int playerIndex;
+    private string nextState;
     //Constructor
     public RockThrowState(PlayerState pS)
     {
         playerState = pS;
-        stateID = FSMStateID.A_RockThrow;
+        stateID = FSMStateID.A_FireBolt;
+    }
+
+    public override void EnterStateInit()
+    {
+        playerState.currentQTEAmount = ActiveSpellCards.Instance.spellCards[(int)SpellNames.Rockthrow].qteAmount;
+        if (playerState.player == GameManager.Instance.player1)
+        {
+            playerIndex = 0;
+        }
+        else
+        {
+            playerIndex = 1;
+        }
     }
 
     //Reason
     public override void Reason(PlayerManager player, PlayerManager enemy)
     {
-
+        if (nextState == "Deciding")
+        {
+            playerState.PerformTransition(Transition.NeedDecision);
+        }
+        else if (nextState == "QTE")
+        {
+            playerState.PerformTransition(Transition.Challenge);
+        }
     }
     //Act
     public override void Act(PlayerManager player, PlayerManager enemy)
     {
         if (!playerState.finishedCurrentQTE)
         {
-
+            nextState = "QTE";
         }
         else
         {
+            if (player.GetComponent<QTEHandler>().EvauateQTEResults() == QTEOUTCOMES.Failure)
+            {
+                player.fingers[(int)player.GetRandomFinger()].fingerHP -= ActiveSpellCards.Instance.spellCards[(int)SpellNames.Rockthrow].damageValue;
+            }
+            else if (player.GetComponent<QTEHandler>().EvauateQTEResults() == QTEOUTCOMES.Half)
+            {
+                player.fingers[(int)player.GetRandomFinger()].fingerHP -= ActiveSpellCards.Instance.spellCards[(int)SpellNames.Rockthrow].damageValue;
+            }
+            else if (player.GetComponent<QTEHandler>().EvauateQTEResults() == QTEOUTCOMES.Success)
+            {
+                enemy.fingers[(int)GameManager.Instance.spellsBeingCast[GameManager.Instance.spellIndex, playerIndex].whatFinger].fingerHP -= ActiveSpellCards.Instance.spellCards[(int)SpellNames.Rockthrow].damageValue;
+            }
 
+            nextState = "Deciding";
         }
     }
 
